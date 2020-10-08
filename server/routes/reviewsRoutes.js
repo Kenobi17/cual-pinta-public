eecb87d48c2b145f409f0be82e0139fec54e2cb9
Kenobi17 <matiascorrea420@gmail.com>
2020-10-08 05:02:10 -0300

    Update reviews routes
    Instead of requesting the brewery_id or review_id from the params, it now requests it from the body

--- server/routes/reviewsRoutes.js
+++ server/routes/reviewsRoutes.js
@@ -3,18 +3,18 @@ const router = require("express").Router(),
   middleware = require("../middleware/middleware");
 
 //CREATE NEW REVIEW ROUTE
-router.post("/new/:brewery_id", middleware.isAuthorized, async (req, res) => {
+router.post("/new/", middleware.isAuthorized, async (req, res) => {
   try {
     const review = await db.query(
       "SELECT * FROM reviews WHERE user_id = $1 AND brewery_id = $2",
-      [req.user.id, req.params.brewery_id]
+      [req.user.id, req.body.brewery_id]
     );
     if (review.rows.length !== 0) {
       return res.json("Ya has realizado una reseña sobre este lugar");
     }
     const newReview = await db.query(
       "INSERT INTO reviews (user_id, brewery_id, body, rating) VALUES ($1, $2, $3, $4) RETURNING *",
-      [req.user.id, req.params.brewery_id, req.body.body, req.body.rating]
+      [req.user.id, req.body.brewery_id, req.body.body, req.body.rating]
     );
     res.json(newReview.rows[0]);
   } catch (error) {
@@ -24,11 +24,11 @@ router.post("/new/:brewery_id", middleware.isAuthorized, async (req, res) => {
 });
 
 //UPDATE REVIEW ROUTE
-router.put("/update/:review_id", middleware.isAuthorized, async (req, res) => {
+router.put("/update/", middleware.isAuthorized, async (req, res) => {
   try {
     const updateReview = await db.query(
       "UPDATE reviews SET body = $1, rating = $2 WHERE review_id = $3 AND user_id = $4 RETURNING *",
-      [req.body.body, req.body.rating, req.params.review_id, req.user.id]
+      [req.body.body, req.body.rating, req.body.review_id, req.user.id]
     );
     if (updateReview.rows.length === 0) {
       return res.json("Esta reseña no es tuya");
@@ -41,24 +41,20 @@ router.put("/update/:review_id", middleware.isAuthorized, async (req, res) => {
 });
 
 //DELETE REVIEW ROUTE
-router.delete(
-  "/delete/:review_id",
-  middleware.isAuthorized,
-  async (req, res) => {
-    try {
-      const deleteReview = await db.query(
-        "DELETE FROM reviews WHERE review_id = $1 AND user_id = $2 RETURNING *",
-        [req.params.review_id, req.user.id]
-      );
-      if (deleteReview.rows.length === 0) {
-        return res.json("Esta reseña no es tuya");
-      }
-      res.json("Reseña eliminada");
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Error del servidor");
+router.delete("/delete/", middleware.isAuthorized, async (req, res) => {
+  try {
+    const deleteReview = await db.query(
+      "DELETE FROM reviews WHERE review_id = $1 AND user_id = $2 RETURNING *",
+      [req.body.review_id, req.user.id]
+    );
+    if (deleteReview.rows.length === 0) {
+      return res.json("Esta reseña no es tuya");
     }
+    res.json("Reseña eliminada");
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Error del servidor");
   }
-);
+});
 
 module.exports = router;
