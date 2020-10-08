9daa64b3cadff55a57ade7bb256d09ecc813a896
Kenobi17 <matiascorrea420@gmail.com>
2020-10-08 20:07:01 -0300

    Add axios module and update breweries route
    Breweries route now makes an API call (using axios and the instagram from each brewerery) to get an image for each brewery and sending that image link in the response

--- server/routes/breweriesRoutes.js
+++ server/routes/breweriesRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router(),
-  db = require("../database/db");
+  db = require("../database/db"),
+  axios = require("axios");
 
 //GET ALL BREWERIES ROUTE
 router.get("/", async (req, res) => {
@@ -7,11 +8,21 @@ router.get("/", async (req, res) => {
     const breweries = await db.query(
       "SELECT * FROM breweries b LEFT JOIN (SELECT brewery_id AS brewery, COUNT(*) AS reviews, TRUNC(AVG(rating), 1) AS rating_avg FROM reviews GROUP BY brewery_id) r ON b.brewery_id = r.brewery ORDER BY b.brewery_id;"
     );
+    //CREATES A NEW ARRAY WHICH CONTAINS ALL THE INFO OF EACH BREWERY AND
+    //ALSO FETCHS EVERY IMAGE FROM THEIR INSTAGRAM
+    const breweriesData = await Promise.all(
+      breweries.rows.map(async (brewery) => {
+        const response = await axios.get(`${brewery.instagram}?__a=1`);
+        const logo = await response.data.graphql.user.profile_pic_url_hd;
+        const data = { ...brewery, image: logo };
+        return data;
+      })
+    );
     res.status(200).json({
       status: "success",
       cantidad: breweries.rowCount,
       data: {
-        cervecerias: breweries.rows,
+        cervecerias: breweriesData,
       },
     });
   } catch (error) {
@@ -31,10 +42,16 @@ router.get("/:id", async (req, res) => {
       "SELECT * FROM reviews WHERE brewery_id = $1",
       [id]
     );
+    //GET BREWERY IMAGE
+    const response = await axios.get(`${brewery.rows[0].instagram}?__a=1`);
+    const logo = await response.data.graphql.user.profile_pic_url_hd;
+    //CREATES A NEW BREWERY OBJECT CONTAINING ALL THE DATA FROM THE BREWERY
+    //AND THE IMAGE
+    const breweryData = { ...brewery.rows[0], image: logo };
     res.status(200).json({
       status: "success",
       data: {
-        cerveceria: brewery.rows[0],
+        cerveceria: breweryData,
         reseñas: reviews.rows,
       },
     });
