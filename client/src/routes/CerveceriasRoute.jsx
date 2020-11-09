38200b84975fd8fbb855aa84b0b4039977e461b4
Kenobi17 <matiascorrea420@gmail.com>
2020-11-09 01:09:55 -0300

    Edit CerveceriasRoute page and add Cervecerias.css
    Began to work on the cerveceriasroute page and also added it's respective css file. Design is almost done, trying to figure out how I will display the search bar and filter

--- client/src/routes/CerveceriasRoute.jsx
+++ client/src/routes/CerveceriasRoute.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
 import CardCerveceria from "../components/CerveceriaRouteComponents/CardCerveceria";
+import Header from "../components/Header";
 import CerveceriasAPI from "../apis/CerveceriasAPI";
 import Grid from "@material-ui/core/Grid";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Typography from "@material-ui/core/Typography";
+import "../css/Cervecerias.css";
 
 const CerveceriasRoute = () => {
   const [cervecerias, setCervecerias] = useState([]);
@@ -18,35 +24,73 @@ const CerveceriasRoute = () => {
     FetchData();
   }, []);
   return (
-    <Grid container justify="center">
-      <Grid item xs={12}>
-        <Typography variant="h3" component="h1" align="center">
-          Cervecerias
-        </Typography>
-      </Grid>
-      <Grid container item xs={10} spacing={1} justify="space-evenly">
-        <Grid style={{ backgroundColor: "#52575d" }} item xs={12} sm={4}></Grid>
-        <Grid item container xs={12} sm={8} spacing={1}>
-          {cervecerias.map((cerveceria) => {
-            return (
-              <Grid
-                key={cerveceria.brewery_id}
-                item
-                xs={12}
-                sm={6}
-                md={4}
-                xl={3}>
-                <CardCerveceria
-                  image={cerveceria.image}
-                  title={cerveceria.name}
-                  {...cerveceria}
-                />
+    <div className="CerveceriasRoute">
+      <Header />
+      <Grid container justify="center" className="mainContainer">
+        <Grid container item xs={10} spacing={1} justify="space-evenly">
+          <Grid item xs={12}>
+            <Typography variant="h3" component="h1" align="center">
+              Cervecerias
+            </Typography>
+          </Grid>
+          <Grid item container xs={12} spacing={3}>
+            <Grid item container xs={12} justify="center">
+              <Grid id="filterGrid" item xs={12} md={10}>
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header">
+                    <Typography
+                      variant="h4"
+                      component="h2"
+                      align="left"
+                      justify-self="left">
+                      Filtrar Cervecerias
+                    </Typography>
+                  </AccordionSummary>
+                  <AccordionDetails id="accordionDetails">
+                    <Typography>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Suspendisse malesuada lacus ex, sit amet blandit leo
+                      lobortis eget.
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
               </Grid>
-            );
-          })}
+              <Grid id="searchForm" item xs={12} md={10}>
+                <form noValidate autoComplete="off">
+                  <Typography
+                    variant="h2"
+                    component="h2"
+                    align="left"
+                    justify-self="left">
+                    Buscar cervecerias
+                  </Typography>
+                  <div className="search">
+                    <input type="text" className="searchTerm" />
+                    <button type="submit" className="searchButton">
+                      <i className="fa fa-search"></i>
+                    </button>
+                  </div>
+                </form>
+              </Grid>
+            </Grid>
+            {cervecerias.map((cerveceria) => {
+              return (
+                <Grid key={cerveceria.brewery_id} item xs={12} sm={6} md={3}>
+                  <CardCerveceria
+                    image={cerveceria.image}
+                    title={cerveceria.name}
+                    {...cerveceria}
+                  />
+                </Grid>
+              );
+            })}
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 };
 
