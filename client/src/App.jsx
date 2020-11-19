83ee25cc79aa5a61d82e45cef0958bd7ca54939e
Kenobi17 <matiascorrea420@gmail.com>
2020-11-19 10:23:36 -0300

    Add authentication logic to App.jsx
    added logic to check if a user is authenticated or not

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
-
+//AUTHENTICATION LOGIC
+import authentication from "./apis/authentication";
 //ROUTES
 import CerveceriasRoute from "./routes/CerveceriasRoute";
 import IndexRoute from "./routes/IndexRoute";
 import RegisterRoute from "./routes/RegisterRoute";
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
+  const isAuth = async () => {
+    try {
+      const response = await authentication.get("/verify", {
+        headers: { token: localStorage.token },
+      });
+      response.data === true
+        ? setIsAuthenticated(true)
+        : setIsAuthenticated(false);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  useEffect(() => {
+    isAuth();
+  });
   return (
     <div className="App">
       <Router>
@@ -24,7 +44,13 @@ function App() {
           <Route
             exact
             path="/register"
-            render={(props) => <RegisterRoute {...props} />}
+            render={(props) =>
+              !isAuthenticated ? (
+                <RegisterRoute {...props} setAuth={setAuth} />
+              ) : (
+                <Redirect to="/cervecerias" />
+              )
+            }
           />
         </Switch>
       </Router>
