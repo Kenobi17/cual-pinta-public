b6483cec7f392b9cfd3be4bebb469bd9e85a3144
Kenobi17 <matiascorrea420@gmail.com>
2021-02-10 20:51:47 -0300

    Update CerveceriaPageRoute
    It now displays the name of the brewerie in the middle of the page. Also, if the id in the params is not a number or doesn't exist, it redirects to /cervecerias

--- client/src/routes/CerveceriaPageRoute.jsx
+++ client/src/routes/CerveceriaPageRoute.jsx
@@ -1,26 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import CerveceriasAPI from "../apis/CerveceriasAPI";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
 const CerveceriaPageRoute = () => {
+  let history = useHistory();
   const { id } = useParams();
   const [cerveceria, setCerveceria] = useState({});
-  const FetchData = async () => {
-    try {
-      const response = await CerveceriasAPI.get(`/${id}`);
-      setCerveceria(response.data.data.cerveceria);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   useEffect(() => {
+    if (isNaN(id) === true) {
+      history.push("/cervecerias");
+    }
+    const FetchData = async () => {
+      try {
+        const response = await CerveceriasAPI.get(`/${id}`);
+        typeof response.data.data.cerveceria === "undefined"
+          ? history.push("/cervecerias")
+          : setCerveceria(response.data.data.cerveceria);
+      } catch (error) {
+        console.log(error);
+      }
+    };
     FetchData();
-  }, []);
+  }, [id, history]);
   return (
     <div className="CerveceriaPageRoute">
-      <div></div>
+      <Grid container justify="center" className="mainContainer">
+        <Grid container item xs={10} justify="center">
+          <Grid item xs={12}>
+            <Typography variant="h3" component="h1" align="center">
+              {cerveceria.name}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 };
