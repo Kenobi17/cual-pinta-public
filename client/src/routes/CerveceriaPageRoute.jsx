c1560550fb2b59a019a7df70facc3cb259fa5fdd
Kenobi17 <matiascorrea420@gmail.com>
2021-03-02 18:47:23 -0300

    Add condition to display or not the addReview component if the user already has a review

--- client/src/routes/CerveceriaPageRoute.jsx
+++ client/src/routes/CerveceriaPageRoute.jsx
@@ -3,6 +3,7 @@ import { useParams, useHistory } from "react-router-dom";
 import CerveceriaDataGrid from "../components/CerveceriaDataGrid";
 import AddReview from "../components/AddReview";
 import CerveceriasAPI from "../apis/CerveceriasAPI";
+import ReviewsAPI from "../apis/ReviewsAPI";
 import Grid from "@material-ui/core/Grid";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Typography from "@material-ui/core/Typography";
@@ -11,6 +12,7 @@ import "../css/CerveceriaPage.css";
 const CerveceriaPageRoute = ({ isAuthenticated }) => {
   let history = useHistory();
   const { id } = useParams();
+  const [hasReview, setHasReview] = useState({});
   const [cerveceriaData, setCerveceriaData] = useState({});
   useEffect(() => {
     if (isNaN(id) === true) {
@@ -22,11 +24,31 @@ const CerveceriaPageRoute = ({ isAuthenticated }) => {
         typeof response.data.data.cerveceria === "undefined"
           ? history.push("/cervecerias")
           : setCerveceriaData(response.data.data);
+        console.log(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const checkReview = async () => {
+      try {
+        const response = await ReviewsAPI.post(
+          "/check",
+          {
+            brewery_id: id,
+          },
+          {
+            headers: {
+              token: localStorage.token,
+            },
+          }
+        );
+        setHasReview(response.data);
       } catch (error) {
         console.log(error);
       }
     };
     FetchData();
+    checkReview();
   }, [id, history]);
   return (
     <div className="CerveceriaPageRoute">
@@ -34,9 +56,9 @@ const CerveceriaPageRoute = ({ isAuthenticated }) => {
         {cerveceriaData.cerveceria ? (
           <>
             <CerveceriaDataGrid cerveceria={cerveceriaData.cerveceria} />
-            {isAuthenticated ? (
+            {isAuthenticated && !hasReview ? (
               <AddReview cerveceria={cerveceriaData.cerveceria} />
-            ) : (
+            ) : isAuthenticated && hasReview ? null : (
               <Typography
                 variant="subtitle2"
                 component="h2"
