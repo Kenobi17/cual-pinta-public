211fd08c14ea656ccd96b1f0dbe871aa3acc4621
Kenobi17 <matiascorrea420@gmail.com>
2021-03-01 23:02:19 -0300

    Add functionality to the AddRevie's form
    Made the inputs a controlled component and also added the function to correctly insert the review into the database using axios post request

--- client/src/components/AddReview.jsx
+++ client/src/components/AddReview.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import ReviewsAPI from "../apis/ReviewsAPI";
 
 const AddReview = ({ cerveceria }) => {
+  const [inputsValues, setInputsValues] = useState({
+    reviewBody: "",
+    reviewRating: "1",
+  });
+
+  const handleChange = (e) => {
+    const { value, name, type, checked } = e.currentTarget;
+    type === "checkbox"
+      ? setInputsValues({ ...inputsValues, [name]: checked })
+      : setInputsValues({ ...inputsValues, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await ReviewsAPI.post(
+        "/new",
+        {
+          brewery_id: cerveceria.brewery_id,
+          body: inputsValues.reviewBody,
+          rating: inputsValues.reviewRating,
+        },
+        {
+          headers: {
+            token: localStorage.token,
+          },
+        }
+      );
+      console.log(response.data);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
   return (
     <Grid item container xs={12} justify="center" id="addReviewGrid">
       <Typography variant="subtitle1" component="h3" align="center">
@@ -12,30 +45,66 @@ const AddReview = ({ cerveceria }) => {
       </Typography>
       <form autoComplete="off">
         <div className="textDiv">
-          <textarea id="reviewTextArea"></textarea>
+          <textarea
+            id="reviewTextArea"
+            placeholder="Dejá tu opinión acerca de la comida, birra, servicio, etc..."
+            onChange={handleChange}
+            value={inputsValues.reviewBody}
+            minLength="15"
+            name="reviewBody"></textarea>
         </div>
         <Typography variant="subtitle1" component="h3">
           Puntaje
         </Typography>
         <div className="ratingDiv">
           <label className="container">
-            <input type="radio" name="rating" value="1" />
+            <input
+              type="radio"
+              onChange={handleChange}
+              value="1"
+              checked={inputsValues.reviewRating === "1"}
+              name="reviewRating"
+            />
             <span className="radioButton"></span> 1
           </label>
           <label className="container">
-            <input type="radio" name="rating" value="2" />
+            <input
+              type="radio"
+              onChange={handleChange}
+              value="2"
+              checked={inputsValues.reviewRating === "2"}
+              name="reviewRating"
+            />
             <span className="radioButton"></span> 2
           </label>
           <label className="container">
-            <input type="radio" name="rating" value="3" />
+            <input
+              type="radio"
+              onChange={handleChange}
+              value="3"
+              checked={inputsValues.reviewRating === "3"}
+              name="reviewRating"
+            />
             <span className="radioButton"></span> 3
           </label>
           <label className="container">
-            <input type="radio" name="rating" value="4" />
+            <input
+              type="radio"
+              onChange={handleChange}
+              value="4"
+              checked={inputsValues.reviewRating === "4"}
+              name="reviewRating"
+            />
             <span className="radioButton"></span> 4
           </label>
           <label className="container">
-            <input type="radio" name="rating" value="5" />
+            <input
+              type="radio"
+              onChange={handleChange}
+              value="5"
+              checked={inputsValues.reviewRating === "5"}
+              name="reviewRating"
+            />
             <span className="radioButton"></span> 5
           </label>
         </div>
@@ -44,6 +113,7 @@ const AddReview = ({ cerveceria }) => {
           variant="contained"
           id="submitReview"
           size="small"
+          onClick={handleSubmit}
           startIcon={<i className="fas fa-beer"></i>}>
           Listo
         </Button>
