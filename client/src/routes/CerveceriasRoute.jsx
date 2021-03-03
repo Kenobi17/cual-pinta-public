bc60b221679fde9b021dc42ad98435b95398455e
Kenobi17 <matiascorrea420@gmail.com>
2021-03-03 15:47:23 -0300

    Add CircularProgress and IsLoading
    Added CircularProgress component to render meanwhile the page is loading, also added that if no brewery has been found when searching, it will show that message

--- client/src/routes/CerveceriasRoute.jsx
+++ client/src/routes/CerveceriasRoute.jsx
@@ -4,10 +4,12 @@ import Search from "../components/CerveceriaRouteComponents/Search";
 import CerveceriasAPI from "../apis/CerveceriasAPI";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import "../css/Cervecerias.css";
 
 const CerveceriasRoute = () => {
   const [cervecerias, setCervecerias] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const filterBreweries = (cerveceriasFilter) => {
     setCervecerias(cerveceriasFilter);
   };
@@ -15,6 +17,7 @@ const CerveceriasRoute = () => {
     try {
       const response = await CerveceriasAPI.get("/");
       setCervecerias(response.data.data.cervecerias);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -44,13 +47,26 @@ const CerveceriasRoute = () => {
               lg={9}
               spacing={3}
               style={{ margin: 0 }}>
-              {cervecerias.map((cerveceria) => {
-                return (
-                  <Grid key={cerveceria.brewery_id} item xs={12} sm={6} md={3}>
-                    <CardCerveceria {...cerveceria} />
-                  </Grid>
-                );
-              })}
+              {isLoading ? (
+                <CircularProgress style={{ color: "#F6C90E" }} />
+              ) : cervecerias.length === 0 ? (
+                <Typography variant="h6" component="h4" align="center">
+                  No se a encontrado ninguna cerveceria
+                </Typography>
+              ) : (
+                cervecerias.map((cerveceria) => {
+                  return (
+                    <Grid
+                      key={cerveceria.brewery_id}
+                      item
+                      xs={12}
+                      sm={6}
+                      md={3}>
+                      <CardCerveceria {...cerveceria} />
+                    </Grid>
+                  );
+                })
+              )}
             </Grid>
           </Grid>
         </Grid>
