444ff77002014e16db407e23883e30c25787c1ca
Kenobi17 <matiascorrea420@gmail.com>
2021-03-05 12:57:47 -0300

    Fix typo

--- client/src/apis/authentication.js
+++ client/src/apis/authentication.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL =
-  process.enf.NODE_ENV === "production"
+  process.env.NODE_ENV === "production"
     ? "authentication"
     : "http://localhost:4000/authentication";
 
